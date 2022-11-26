19521da4cc215537a961fd14afa3651b05c4bb18
moonlex <m00n-lex@yandex.ru>
2022-11-26 14:35:32 +0500

    ChangeDisplayFlex

---
+++ Client/js/app.ts
@@ -0,0 +1,47 @@
+// const rotateCube = () => {
+//     document.getElementById('main').classList.add('rotatedCube') // not real class
+// }
+//
+// document.getElementById('main').addEventListener('click', rotateCube)
+
+// - - - - - DEV IDEA PART END - - - - - //
+
+console.log('It\'s working bruh' as string)
+
+// function offVisibility(elem: string){
+//     const elemDOM = document.getElementById(elem);
+//     if(elemDOM){
+//         elemDOM.style.visibility = 'hidden'
+//     }
+//     return;
+// }
+
+const changeVisibility = (elem: string) => {
+    let elemDOM = document.getElementById(elem);
+    if(elemDOM){
+        (elemDOM.style.visibility === 'hidden')?elemDOM.style.visibility = 'visible':elemDOM.style.visibility = 'hidden';
+    }
+}
+
+const changeDisplayFlex = (displayElem: string, mode: string) => {
+    let elemDOM = document.getElementById(displayElem);
+    if(elemDOM){
+        switch (mode){
+            case 'on':
+                elemDOM.style.display = 'flex';
+                break
+            case 'off':
+                elemDOM.style.display = 'none'
+                break
+            default: // DEV
+                console.log(`ERROR: Wrong argument, waited for on/off, took ${mode}`)// DEV
+                break// DEV
+        }
+    }
+}
+changeDisplayFlex('darkStartWindow', 'on'); //!
+
+setTimeout(function (){
+    changeDisplayFlex('darkStartWindow', 'off');
+    changeDisplayFlex('playBtnWindow', 'on')
+    }, 4000)
